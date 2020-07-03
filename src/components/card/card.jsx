bb6f751031059dadc880b56e9da6b56b220c3f51
VieiraMidas <guilherme.brasil3@etec.sp.gov.br>
2020-07-03 14:51:02 -0300

    axios add e busca de inf na api funcionando

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './card.css'
 
-export default function Card(){
+export default function Card({music}){
 	return (
 		<div className="card-serie">
 			<div>
-				<span className="material-icons fav-btn">favorite_border</span>
-				<div className="poster-image"></div>
+				<span className="material-icons inf-btn">info</span>
+				<div className="poster-image"
+				style= {{ backgroundImage: `url(${music.pic_src}`}}></div>
 				<div className="card-detail">
-					<h1 id="musica">Happier</h1>
-					<h2 class="sub-itens">Marshmallow</h2>
-					<h2 class="sub-itens">Eletrônica</h2>
+					<h1 id="musica">{music.musTitle}</h1>
+					<h2 class="sub-itens">{music.title}</h2>
+					<h2 class="sub-itens">{music.date_fmt}</h2>
 				</div>
 			</div>
 		</div>
--- src/pages/novidades/novidades.jsx
+++ src/pages/novidades/novidades.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
+import React, {Fragment,useEffect, useState} from 'react'
+import Axios from 'axios'
+
 import CardSerie from "../../components/card/card"
 import Top from "../../components/TopHome/index"
 import Carrossel from "../../components/carrossel/carrossel"
 
+
 export default function Novidades(){
+
+    const [musics, setMusics] = useState([])
+    
+    useEffect(() => load(), [])
+
+    async function load(){
+        try{
+        const resposta = await Axios.get("http://api.vagalume.com.br/hotspots.php?apikey=%7Bkey%7D&language=pt-BR")
+        setMusics(resposta.data.hotspots)
+        console.log(resposta.data.hotspots)
+        }catch(e){
+            console.log(e)
+        }
+    }
+    
     return(
-        <>
+        <Fragment>
             <Top />
             <h1 id="h1">+ Acessados</h1>
             <div className="content">
-      	        <CardSerie />
-      	        <CardSerie />
-      	        <CardSerie />
-      	        <CardSerie />
+                {musics.map((music) => <CardSerie key={music.id} music={music}/>)}
             </div>
             <h1 id="h1">+ Notícias</h1>
             <Carrossel />
             <p>more</p>
-        </>
+        </Fragment>
     );
 }
